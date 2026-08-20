40558e8388f66806cd24bd875ef3c451df7e8a4d
Kartik Sharma <myselfkari57@gmail.com>
2026-08-20 11:35:39 -0400

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        return Math.max(robLinear(nums, 0, nums.length-2),robLinear(nums, 1, nums.length-1));}
+
+
+
+        private int robLinear(int[] nums, int start, int end){
+            int rob1= 0;
+            int rob2=0;
+            for(int i=start; i<=end ; i++){
+                int sum= Math.max(nums[i]+rob1, rob2);
+                rob1=rob2;
+                rob2= sum;
+            }
+            return rob2;
+        }
+        
+    }
+
