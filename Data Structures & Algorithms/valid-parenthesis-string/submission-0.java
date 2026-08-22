03b71dff3f0462638724cdab6731d8d14e65f808
Kartik Sharma <myselfkari57@gmail.com>
2026-08-21 21:42:50 -0400

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        for(int i = 0 ; i<s.length() ; i++){
+            char c= s.charAt(i);
+            if( c== '('){
+                stack1.push(i);
+            }
+           else if( c== '*'){
+                stack2.push(i);
+            }
+            else{
+                if(!stack1.isEmpty()){
+                    stack1.pop();
+                }
+                else if(!stack2.isEmpty()){
+                    stack2.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+
+        }
+        while(!stack1.isEmpty() && !stack2.isEmpty()){
+            if(stack1.pop()> stack2.pop()) return false;
+            }
+            return stack1.isEmpty();
+        }
+
+        
+    }
+
